9f78e397d14fc026d92c57f7d6e5d8bc0c6f232e
eefremova2805 <evgeniia.efremova@athleticgreens.com>
2024-01-30 13:47:25 +0700

    add tg bot for orders

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -3,6 +3,7 @@ import { Button } from './shared/Button/Button';
 import { CartContext } from '../context/CartContext';
 import { useNavigate } from 'react-router-dom';
 import { ModalContext } from '../context/ModalContext';
+import axios from 'axios';
 
 const CheckoutForm: React.FC = () => {
   const [name, setName] = useState('');
@@ -11,13 +12,32 @@ const CheckoutForm: React.FC = () => {
   const { clearCart } = useContext(CartContext);
   const navigate = useNavigate();
 
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
+  
+    clearCart();
+    navigate('/thank-you');
+  
+    await sendOrderNotification();
+  };
 
-    setTimeout(() => {
-      clearCart();
-      navigate('/thank-you');
-    }, 2000);
+  const sendOrderNotification = async () => {
+    const botToken = '6829546671:AAFdTro-aYwuC7zF-CwCaVkW4aQ7WqihZww';
+    const chatId = '@portfolio_orders';
+    const message = `New order received! \nName: ${name}\nAddress: ${address}`;
+  
+    try {
+      await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+        chat_id: chatId,
+        text: message
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
   };
 
   const {close} = useContext(ModalContext);
@@ -38,7 +58,7 @@ const CheckoutForm: React.FC = () => {
   return (
     <form onSubmit={handleSubmit}>
       <label>
-                Name:
+        Name:
         <input type="text" 
           value={name} 
           required
@@ -48,7 +68,7 @@ const CheckoutForm: React.FC = () => {
       </label>
       <br />
       <label>
-                Address:
+        Address:
         <input type="text" 
           value={address} 
           required
