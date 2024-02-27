7d6c6ce69af8eb530e0d6ea2388361b185f23e23
eefremova2805 <evgeniia.efremova@athleticgreens.com>
2024-02-27 20:34:46 +0700

    delete commented code

--- src/utils/sendOrderNotification.ts
+++ src/utils/sendOrderNotification.ts
@@ -4,7 +4,6 @@ import { CartItem } from "../models";
 export const sendOrderNotification = async (name: string, address: string, cartItems: CartItem[], totalPrice: number, totalQuantaty: number) => {
 
     const botToken = process.env.BOT_TOKEN;
-    // const chatId = '@portfolio_orders';
     const chatId = process.env.CHAT_ID;
     const cartDetails = cartItems.map(item => `${item.title} (Qty: ${item.quantity})`).join('\n');
     const message = `New order received! \n\nName: ${name}\nAddress: ${address} \n\n${cartDetails} \n\nTotal: ${totalQuantaty} items for $${Math.round(totalPrice)}`;
